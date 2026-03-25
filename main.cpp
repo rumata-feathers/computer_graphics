57274d1a37e4e0fbaf2614ceafa497d9f9fe3927
rumata-feathers <georgii.kuznetsov@polytechnique.edu>
2026-03-25 15:54:26 +0100

    lab1 submission

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include <vector>
 #include <cmath>
 #include <random>
+#include <vector>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
@@ -19,220 +19,306 @@ static std::uniform_real_distribution<double> uniform(0, 1);
 double sqr(double x) { return x * x; };
 
 class Vector {
-public:
-	explicit Vector(double x = 0, double y = 0, double z = 0) {
-		data[0] = x;
-		data[1] = y;
-		data[2] = z;
-	}
-	double norm2() const {
-		return data[0] * data[0] + data[1] * data[1] + data[2] * data[2];
-	}
-	double norm() const {
-		return sqrt(norm2());
-	}
-	void normalize() {
-		double n = norm();
-		data[0] /= n;
-		data[1] /= n;
-		data[2] /= n;
-	}
-	double operator[](int i) const { return data[i]; };
-	double& operator[](int i) { return data[i]; };
-	double data[3];
+   public:
+    explicit Vector(double x = 0, double y = 0, double z = 0) {
+        data[0] = x;
+        data[1] = y;
+        data[2] = z;
+    }
+    double norm2() const {
+        return data[0] * data[0] + data[1] * data[1] + data[2] * data[2];
+    }
+    double norm() const { return sqrt(norm2()); }
+    void normalize() {
+        double n = norm();
+        data[0] /= n;
+        data[1] /= n;
+        data[2] /= n;
+    }
+    double operator[](int i) const { return data[i]; };
+    double& operator[](int i) { return data[i]; };
+    double data[3];
 };
 
 Vector operator+(const Vector& a, const Vector& b) {
-	return Vector(a[0] + b[0], a[1] + b[1], a[2] + b[2]);
+    return Vector(a[0] + b[0], a[1] + b[1], a[2] + b[2]);
 }
 Vector operator-(const Vector& a, const Vector& b) {
-	return Vector(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
+    return Vector(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
 }
 Vector operator*(const double a, const Vector& b) {
-	return Vector(a*b[0], a*b[1], a*b[2]);
+    return Vector(a * b[0], a * b[1], a * b[2]);
 }
 Vector operator*(const Vector& a, const double b) {
-	return Vector(a[0]*b, a[1]*b, a[2]*b);
+    return Vector(a[0] * b, a[1] * b, a[2] * b);
 }
 Vector operator/(const Vector& a, const double b) {
-	return Vector(a[0] / b, a[1] / b, a[2] / b);
+    return Vector(a[0] / b, a[1] / b, a[2] / b);
 }
 double dot(const Vector& a, const Vector& b) {
-	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
 }
 Vector cross(const Vector& a, const Vector& b) {
-	return Vector(a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]);
+    return Vector(a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2],
+                  a[0] * b[1] - a[1] * b[0]);
 }
 
 class Ray {
-public:
-	Ray(const Vector& origin, const Vector& unit_direction) : O(origin), u(unit_direction) {};
-	Vector O, u;
+   public:
+    Ray(const Vector& origin, const Vector& unit_direction)
+        : O(origin), u(unit_direction) {};
+    Vector O, u;
 };
 
 class Object {
-public:
-	Object(const Vector& albedo, bool mirror = false, bool transparent = false) : albedo(albedo), mirror(mirror), transparent(transparent) {};
+   public:
+    Object(const Vector& albedo, bool mirror = false, bool transparent = false)
+        : albedo(albedo), mirror(mirror), transparent(transparent) {};
 
-	virtual bool intersect(const Ray& ray, Vector& P, double& t, Vector& N) const = 0;
+    virtual bool intersect(const Ray& ray, Vector& P, double& t,
+                           Vector& N) const = 0;
 
-	Vector albedo;
-	bool mirror, transparent;
+    Vector albedo;
+    bool mirror, transparent;
 };
 
 class Sphere : public Object {
-public:
-	Sphere(const Vector& center, double radius, const Vector& albedo, bool mirror = false, bool transparent = false) : ::Object(albedo, mirror, transparent), C(center), R(radius) {};
+   public:
+    Sphere(const Vector& center, double radius, const Vector& albedo,
+           bool mirror = false, bool transparent = false)
+        : ::Object(albedo, mirror, transparent), C(center), R(radius) {};
 
-	// returns true iif there is an intersection between the ray and the sphere
-	// if there is an intersection, also computes the point of intersection P, 
-	// t>=0 the distance between the ray origin and P (i.e., the parameter along the ray)
-	// and the unit normal N
-	bool intersect(const Ray& ray, Vector& P, double &t, Vector& N) const {
-		 // TODO (lab 1) : compute the intersection (just true/false at the begining of lab 1, then P, t and N as well)
-		return false;
-	}
+    // returns true iif there is an intersection between the ray and the sphere
+    // if there is an intersection, also computes the point of intersection P,
+    // t>=0 the distance between the ray origin and P (i.e., the parameter along
+    // the ray) and the unit normal N
+    bool intersect(const Ray& ray, Vector& P, double& t, Vector& N) const {
+        // TODO (lab 1) : compute the intersection (just true/false at the
+        // begining of lab 1, then P, t and N as well) perf first, calculate
+        // product
+        auto dot_prod = dot(ray.u, ray.O - C);
+        auto delta = sqr(dot_prod) - ((ray.O - C).norm2() - sqr(R));
 
-	double R;
-	Vector C;
+        if (delta < 0) return false;
+
+        auto disc = sqrt(delta);
+
+        auto t1 = -dot_prod - disc;
+        auto t2 = -dot_prod + disc;
+
+        if (t2 < 0) return false;  // ray looking in other direction
+
+        t = t1 >= 0 ? t1 : t2;  // keep minimal positive t
+
+        P = ray.O + t * ray.u;
+        N = P - C;
+        N.normalize();
+
+        return true;
+    }
+
+    double R;
+    Vector C;
 };
-
 
 // I will provide you with an obj mesh loader (labs 3 and 4)
 class TriangleMesh : public Object {
-public:
-	TriangleMesh(const Vector& albedo, bool mirror = false, bool transparent = false) : ::Object(albedo, mirror, transparent) {};
+   public:
+    TriangleMesh(const Vector& albedo, bool mirror = false,
+                 bool transparent = false)
+        : ::Object(albedo, mirror, transparent) {};
 
-	bool intersect(const Ray& ray, Vector& P, double& t, Vector& N) const {
-		// TODO (labs 3 and 4)
-		return false;
-	}
+    bool intersect(const Ray& ray, Vector& P, double& t, Vector& N) const {
+        // TODO (labs 3 and 4)
+        return false;
+    }
 };
-
 
 class Scene {
-public:
-	Scene() {};
-	void addObject(const Object* obj) {
-		objects.push_back(obj);
-	}
+   public:
+    Scene() {};
+    void addObject(const Object* obj) { objects.push_back(obj); }
 
-	// returns true iif there is an intersection between the ray and any object in the scene
-    // if there is an intersection, also computes the point of the *nearest* intersection P, 
-    // t>=0 the distance between the ray origin and P (i.e., the parameter along the ray)
-    // and the unit normal N. 
-	// Also returns the index of the object within the std::vector objects in object_id
-	bool intersect(const Ray& ray, Vector& P, double& t, Vector& N, int &object_id) const  {
+    // returns true iif there is an intersection between the ray and any object
+    // in the scene
+    // if there is an intersection, also computes the point of the *nearest*
+    // intersection P, t>=0 the distance between the ray origin and P (i.e., the
+    // parameter along the ray) and the unit normal N. Also returns the index of
+    // the object within the std::vector objects in object_id
+    bool intersect(const Ray& ray, Vector& P, double& t, Vector& N,
+                   int& object_id) const {
+        // TODO (lab 1): iterate through the objects and check the intersections
+        // with all of them, and keep the closest intersection, i.e., the one if
+        // smallest positive value of t
 
-		// TODO (lab 1): iterate through the objects and check the intersections with all of them, 
-		// and keep the closest intersection, i.e., the one if smallest positive value of t
+        double min_t = std::numeric_limits<double>::max();
+        Vector min_P = P;
+        Vector min_N = N;
 
-		return false;
-	}
+        double tmp_t = 0;
+		bool intersected = false;
+        for (size_t i = 0; i < objects.size(); ++i) {
+            if (objects[i]->intersect(ray, min_P, tmp_t, min_N)) {
+				if (tmp_t < min_t){
+					min_t = tmp_t;
+					object_id = i;
+					N = min_N;
+					P = min_P;
+					t = min_t;
+				}
+				intersected = true;
+            }
+        }
+        return intersected;
+    }
 
+    // return the radiance (color) along ray
+    Vector getColor(const Ray& ray, int recursion_depth) {
+        if (recursion_depth >= max_light_bounce) return Vector(0, 0, 0);
 
-	// return the radiance (color) along ray
-	Vector getColor(const Ray& ray, int recursion_depth) {
+        // TODO (lab 1) : if intersect with ray, use the returned information to
+        // compute the color ; otherwise black in lab 1, the color only includes
+        // direct lighting with shadows
 
-		if (recursion_depth >= max_light_bounce) return Vector(0, 0, 0);
+		// return Vector(255, 255, 255);
+        Vector P, N;
+        double t;
+        int object_id;
+        if (intersect(ray, P, t, N, object_id)) {
+			// return Vector(255, 255, 255);
+            if (objects[object_id]->mirror) {
+                // return getColor in the reflected direction, with
+                // recursion_depth+1 (recursively)
+                auto ref_direction = ray.u - 2 * dot(ray.u, N) * N;
+                auto new_ray = Ray(P, ref_direction);
 
-		// TODO (lab 1) : if intersect with ray, use the returned information to compute the color ; otherwise black 
-		// in lab 1, the color only includes direct lighting with shadows
+                return getColor(new_ray, ++recursion_depth);
+            }  // else
 
-		Vector P, N;
-		double t;
-		int object_id;
-		if (intersect(ray, P, t, N, object_id)) {
+            if (objects[object_id]->transparent) {  // optional
 
-			if (objects[object_id]->mirror) {
+                // auto ref_direction = ray.u - 2 * dot(ray.u, N) * N;
+                // auto new_ray = Ray(P, ref_direction);
 
-				// return getColor in the reflected direction, with recursion_depth+1 (recursively)
-			} // else
+                // return getColor(new_ray, ++recursion_depth);
 
-			if (objects[object_id]->transparent) { // optional
+                // return getColor in the refraction direction, with
+                // recursion_depth+1 (recursively)
+            }  // else
 
-				// return getColor in the refraction direction, with recursion_depth+1 (recursively)
-			} // else
+        	// return Vector(255, 255, 255);
+			
 
-			// test if there is a shadow by sending a new ray
-			// if there is no shadow, compute the formula with dot products etc.
+            // test if there is a shadow by sending a new ray
+            Vector direction_to_light = light_position - P;
+            auto distance_to_light = direction_to_light.norm();
+            direction_to_light.normalize();
 
+            auto shadow_ray = Ray(P + 1e-10*N, direction_to_light);
 
-			// TODO (lab 2) : add indirect lighting component with a recursive call
-		}
+            Vector shadow_P, shadow_N;
+            int shadow_id;
+            double shadow_t;
+            bool in_shadow = false;
+            if (intersect(shadow_ray, shadow_P, shadow_t, shadow_N,
+                          shadow_id)) {
+                if (shadow_t < distance_to_light) in_shadow = true;
+            }
 
-		
+            if (in_shadow) return Vector(0, 0, 0);
+            // if there is no shadow, compute the formula with dot products etc.
+        	// return Vector(255, 255, 255);
+            return (light_intensity /
+                    (4 * M_PI * distance_to_light * distance_to_light)) *
+                   (objects[object_id]->albedo / M_PI) *
+                   std::max(0.0, dot(N, direction_to_light));
 
-		return Vector(0, 0, 0);
-	}
+            // TODO (lab 2) : add indirect lighting component with a recursive
+            // call
+        }
 
-	std::vector<const Object*> objects;
+        return Vector(0, 0, 0);
+    }
 
-	Vector camera_center, light_position;
-	double fov, gamma, light_intensity;
-	int max_light_bounce;
+    std::vector<const Object*> objects;
+
+    Vector camera_center, light_position;
+    double fov, gamma, light_intensity;
+    int max_light_bounce;
 };
 
-
 int main() {
-	int W = 512;
-	int H = 512;
+    int W = 512;
+    int H = 512;
 
-	for (int i = 0; i<32; i++) {
-		engine[i].seed(i);
-	}
+    for (int i = 0; i < 32; i++) {
+        engine[i].seed(i);
+    }
 
-	Sphere center_sphere(Vector(0, 0, 0), 10., Vector(0.8, 0.8, 0.8));
-	Sphere wall_left(Vector(-1000, 0, 0), 940, Vector(0.5, 0.8, 0.1));
-	Sphere wall_right(Vector(1000, 0, 0), 940, Vector(0.9, 0.2, 0.3));
-	Sphere wall_front(Vector(0, 0, -1000), 940, Vector(0.1, 0.6, 0.7));
-	Sphere wall_behind(Vector(0, 0, 1000), 940, Vector(0.8, 0.2, 0.9));
-	Sphere ceiling(Vector(0, 1000, 0), 940, Vector(0.3, 0.5, 0.3));
-	Sphere floor(Vector(0, -1000, 0), 990, Vector(0.6, 0.5, 0.7));
+    Sphere center_sphere(Vector(0, 0, 0), 10., Vector(0.8, 0.8, 0.8));
+    Sphere wall_left(Vector(-1000, 0, 0), 940, Vector(0.5, 0.8, 0.1));
+    Sphere wall_right(Vector(1000, 0, 0), 940, Vector(0.9, 0.2, 0.3));
+    Sphere wall_front(Vector(0, 0, -1000), 940, Vector(0.1, 0.6, 0.7));
+    Sphere wall_behind(Vector(0, 0, 1000), 940, Vector(0.8, 0.2, 0.9));
+    Sphere ceiling(Vector(0, 1000, 0), 940, Vector(0.3, 0.5, 0.3));
+    Sphere floor(Vector(0, -1000, 0), 990, Vector(0.6, 0.5, 0.7));
 
-	Scene scene;
-	scene.camera_center = Vector(0, 0, 0);
-	scene.light_position = Vector(-10,20,40);
-	scene.light_intensity = 3E7;
-	scene.fov = 60 * M_PI / 180.;
-	scene.gamma = 1.0;    // TODO (lab 1) : play with gamma ; typically, gamma = 2.2
-	scene.max_light_bounce = 5;
+    Scene scene;
+    scene.camera_center = Vector(0, 0, 55);
+    scene.light_position = Vector(-10, 20, 40);
+    scene.light_intensity = 3E7;
+    scene.fov = 60 * M_PI / 180.;
+    scene.gamma =
+        2.2;  // TODO (lab 1) : play with gamma ; typically, gamma = 2.2
+    scene.max_light_bounce = 5;
 
-	scene.addObject(&center_sphere);
+    scene.addObject(&center_sphere);
 
-	/*
-	scene.addObject(&wall_left);
-	scene.addObject(&wall_right);
-	scene.addObject(&wall_front);
-	scene.addObject(&wall_behind);
-	scene.addObject(&ceiling);
-	scene.addObject(&floor);
-	*/
 
-	std::vector<unsigned char> image(W * H * 3, 0);
+    scene.addObject(&wall_left);
+    scene.addObject(&wall_right);
+    scene.addObject(&wall_front);
+    scene.addObject(&wall_behind);
+    scene.addObject(&ceiling);
+    scene.addObject(&floor);
+    
+
+    std::vector<unsigned char> image(W * H * 3, 0);
 
 #pragma omp parallel for schedule(dynamic, 1)
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
-			Vector color;
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            Vector color;
 
-			// TODO (lab 1) : correct ray_direction so that it goes through each pixel (j, i)			
-			Vector ray_direction(0., 0., -1);
+            // TODO (lab 1) : correct ray_direction so that it goes through each
+            // pixel (j, i)
+            Vector ray_direction(j - W / 2 + 0.5, H / 2 - i - 0.5,
+                                 -W / (2 * tan(scene.fov / 2)));
 
-			Ray ray(scene.camera_center, ray_direction);
+			ray_direction.normalize();
+            Ray ray(scene.camera_center, ray_direction);
 
-			// TODO (lab 2) : add Monte Carlo / averaging of random ray contributions here
-			// TODO (lab 2) : add antialiasing by altering the ray_direction here
-			// TODO (lab 2) : add depth of field effect by altering the ray origin (and direction) here
+            // TODO (lab 2) : add Monte Carlo / averaging of random ray
+            // contributions here
+            // TODO (lab 2) : add antialiasing by altering the ray_direction
+            // here
+            // TODO (lab 2) : add depth of field effect by altering the ray
+            // origin (and direction) here
 
-			color  = scene.getColor(ray, 0);
+            color = scene.getColor(ray, 0);
 
-			image[(i * W + j) * 3 + 0] = std::min(255., std::max(0., 255. * std::pow(color[0] / 255., 1. / scene.gamma)));
-			image[(i * W + j) * 3 + 1] = std::min(255., std::max(0., 255. * std::pow(color[1] / 255., 1. / scene.gamma)));
-			image[(i * W + j) * 3 + 2] = std::min(255., std::max(0., 255. * std::pow(color[2] / 255., 1. / scene.gamma)));
-		}
-	}
-	stbi_write_png("image.png", W, H, 3, &image[0], 0);
+            image[(i * W + j) * 3 + 0] =
+                std::min(255., std::max(0., 255. * std::pow(color[0] / 255.,
+                                                            1. / scene.gamma)));
+            image[(i * W + j) * 3 + 1] =
+                std::min(255., std::max(0., 255. * std::pow(color[1] / 255.,
+                                                            1. / scene.gamma)));
+            image[(i * W + j) * 3 + 2] =
+                std::min(255., std::max(0., 255. * std::pow(color[2] / 255.,
+                                                            1. / scene.gamma)));
+        }
+    }
+    stbi_write_png("image.png", W, H, 3, &image[0], 0);
 
-	return 0;
+    return 0;
 }
